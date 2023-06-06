2c47f877808ed59baf25d7a0a0d520d4973fc900
TV#BJJ <tv041000@gmail.com>
2023-06-06 16:50:33 +1000

    Выполнено дз

--- src/main/java/ru/skypro/lessons/springboot/weblibrary/WebLibraryApplication.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary/WebLibraryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebLibraryHwApplication {
+public class WebLibraryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebLibraryHwApplication.class, args);
+        SpringApplication.run(WebLibraryApplication.class, args);
     }
 
 }
